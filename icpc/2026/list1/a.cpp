d31e390671282852aff18c07994df09e80555e49
ghustdev <gustavocardoso0019@gmail.com>
2026-03-11 14:22:03 -0300

    feat: init ICPC 2026/1

---
+++ icpc/2026/list1/a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+
+typedef long long ll;
+const int MAX = 2e5;
+const int MAXLL = 2e10;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    _;
+    
+    int t = 1;
+    cin >> t;
+
+    while (t--) {
+        solve();
+        cout << endl;
+    }
+
+    return 0;
+}
