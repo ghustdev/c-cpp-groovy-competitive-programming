b13cbf60ca3bcc865d6305d3ed0a7ad3eb47b59b
ghustdev <gustavocardoso0019@gmail.com>
2026-03-18 22:27:50 -0300

    update

--- icpc/2026/list2/a.cpp
+++ icpc/2026/list2/a.cpp
@@ -10,23 +10,28 @@ const ll MAX = 10e5 + 10;
 void solve()
 {
     unordered_map <ll, ll> mp;
-    array
+    vector<ll> nums(MAX);
+
     int n; cin >> n;
     
     ll count = 0;
 
     for (int i = 0; i < n; i++) {
         ll x; cin >> x;
-        mp[x] += 1;
+        if (mp[x] == 0) nums[i] = x;
+        mp[x]++;
     }
 
-    for (int i=1; i<=MAX; i++) {
-        if (mp[i] == 0) {
+    cout << endl;
+
+    for (int i = 0; i < n; i++) {
+        ll x = nums[i];
+        if (mp[x] == 0) {
             continue;
         }
-        cout << mp[i] << " ";
-        if (mp[i] < i) count += mp[i];
-        else count += mp[i] - i;
+
+        if (mp[x] < x) count += mp[x];
+        else count += mp[x] - x;
     }
 
     cout <<  endl << count;
