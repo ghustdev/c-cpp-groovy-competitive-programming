41eb9e07306749cda4b48ea4798280fe052b7d68
ghustdev <gustavocardoso0019@gmail.com>
2026-04-23 11:05:10 -0300

    feat: finishd exercise 2 of the list 2 of the POO

---
+++ poo/lista-2-heranca-abstracao/src/exe2_clientes_1/CadastroMultiplo.java
@@ -0,0 +1,34 @@
+package exe2_clientes_1;
+
+public class CadastroMultiplo {
+	
+	public static void main(String[] args) {
+		
+		Cliente[] cliente = new Cliente[5];
+		
+		cliente[0] = new Cliente("Maria");
+		cliente[0].setIdade(20);
+		cliente[0].setTelefone("999-8899");
+		
+		cliente[1] = new Cliente("Stefanny");
+		cliente[1].setIdade(20);
+		cliente[1].setTelefone("999-8899");
+		
+		cliente[2] = new Cliente("Gustavo");
+		cliente[2].setIdade(20);
+		cliente[2].setTelefone("999-8899");
+		
+		cliente[3] = new Cliente("Rita");
+		cliente[3].setIdade(20);
+		cliente[3].setTelefone("999-8899");
+		
+		cliente[4] = new Cliente("Deri");
+		cliente[4].setIdade(20);
+		cliente[4].setTelefone("999-8899");
+		
+		for (int i=0;i<cliente.length;i++) {
+			System.out.println(i+1 + " " + cliente[i]);
+			
+		}
+	}
+}
